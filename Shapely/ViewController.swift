165d5c45cedde48907701332d9c5578afc9f4977
Nil Point <nilpoint@hotmail.com>
2015-09-13 21:11:38 +0800

    Add square to the main view.

--- Shapely/ViewController.swift
+++ Shapely/ViewController.swift
@@ -20,6 +20,20 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
 
+  @IBAction func addShape(sender: AnyObject!) {
+    if let button = sender as? UIButton {
+      let shapeView = ShapeView(shape: .Square)
+      view.addSubview(shapeView)
+      
+      var shapeFrame = shapeView.frame
+      let safeRect = CGRectInset(view.bounds, shapeFrame.width, shapeFrame.height)
+      
+      var randomCenter = safeRect.origin
+      randomCenter.x += CGFloat(arc4random_uniform(UInt32(safeRect.width)))
+      randomCenter.y += CGFloat(arc4random_uniform(UInt32(safeRect.height)))
+      shapeView.center = randomCenter
+    }
+  }
 
 }
 
