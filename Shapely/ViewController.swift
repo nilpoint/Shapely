9aff7f0ef5cc64b8326d8264fc2ba2bc3787960a
Nil Point <nilpoint@hotmail.com>
2015-09-13 16:29:57 +0800

    Initial Commit

---
+++ Shapely/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Shapely
+//
+//  Created by John Alstru on 9/13/15.
+//  Copyright (c) 2015 nilpoint.sample. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
